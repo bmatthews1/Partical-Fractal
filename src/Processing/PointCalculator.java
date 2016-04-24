b7f65d77fe2f585b75fdb6d201454b7284eb7ce0
Ben Matthews <bmatthews1@unm.edu>
2016-04-23 19:32:17 -0600

    added some processing logic to PointCalculator

--- src/Processing/PointCalculator.java
+++ src/Processing/PointCalculator.java
@@ -18,6 +18,10 @@ public class PointCalculator {
 	
 	private LinkedList<ControlPoint> controls;
 	private LinkedList<RenderPoint> renders;
+	private LinkedList<Double> distances;
+	private LinkedList<Double> angles;
+	private double totalDistance; //distance from first control to last;
+	private double totalAngle; //total angle between first control and last
 	
 	/**
 	 * Contructor for the PointCalculator takes in a list of ControlPoints
@@ -29,13 +33,58 @@ public class PointCalculator {
 	public PointCalculator(LinkedList<ControlPoint> controls, LinkedList<RenderPoint> renders){
 		this.controls = controls;
 		this.renders = renders;
+		distances = new LinkedList<>();
+		angles = new LinkedList<>();
 	}
 	
 	/**
 	 * Public call to calculate the RenderPoint positions and colors
 	 */
 	public void calculatePoints(){
+		distances.clear();
+		angles.clear();
 		//TODO
+		for (int i = 0; i < controls.size()-1; i++){
+			ControlPoint current = controls.get(i);
+			ControlPoint next = controls.get(i+1);
+			
+			double x1 = current.getCenterX();
+			double y1 = current.getCenterY();
+			double x2 = next.getCenterX();
+			double y2 = next.getCenterY();
+			
+			double xDiff = x1 - x2;
+			double yDiff = y1 - y2;
+			
+			double distance = Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+			
+			double angle = Math.atan(yDiff/xDiff);
+			
+			distances.add(distance);
+			
+			angles.add(angle);
+		}
+		
+		ControlPoint start = controls.get(0);
+		ControlPoint end = controls.get(controls.size()-1);
+		
+		double x1 = start.getCenterX();
+		double y1 = start.getCenterY();
+		double x2 = end.getCenterX();
+		double y2 = end.getCenterY();
+		
+		double xDiff = x1 - x2;
+		double yDiff = y1 - y2;
+		
+		totalDistance = Math.sqrt(xDiff*xDiff + yDiff*yDiff);
+		
+		totalAngle = Math.atan(yDiff/xDiff);
+		
+		for (int i = 0; i < angles.size(); i++){
+			Double angle = angles.get(i);
+			angle = angle - totalAngle;
+		}
+		
 	}
 	
 	/**
@@ -47,14 +96,6 @@ public class PointCalculator {
 	 * @param c2 - bound circle 2
 	 */
 	private void addPoints(Circle c1, Circle c2){
-		double x1 = c1.getCenterX();
-		double y1 = c1.getCenterY();
-		double x2 = c2.getCenterX();
-		double y2 = c2.getCenterY();
 		
-		double xDiff = x1 - x2;
-		double yDiff = y1 - y2;
-		
-		double distance = Math.sqrt(xDiff*xDiff + yDiff*yDiff);
 	}
 }
